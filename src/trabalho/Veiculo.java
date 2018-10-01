94e9861fd23d9038d2ab4837d14660054e30dc8a
Filipi <lipi-valim@hotmail.com>
2018-10-01 11:39:56 -0300

    Classe Veiculo

--- src/trabalho/Veiculo.java
+++ src/trabalho/Veiculo.java
@@ -8,4 +8,67 @@ package trabalho;
 
 public class Veiculo {
     
+    public Veiculo() {
+        x = 0; 
+        y = 0; 
+        velocidade = 0; 
+        fabrica = false; 
+        cor = null; 
+	}
+    
+    
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public boolean isFabrica() {
+        return fabrica;
+    }
+
+    public void setFabrica(boolean fabrica) {
+        this.fabrica = fabrica;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setVelocidade(int velocidade) {
+        this.velocidade = velocidade;
+    }
+
+    public int getVelocidade() {
+        return velocidade;
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+        private int x; 
+	private int y; 
+	private int velocidade; 
+	private boolean fabrica; 
+	private String cor; 
+    
 }
